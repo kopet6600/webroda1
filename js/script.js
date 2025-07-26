2817ffa16325e626695077cfd580489512c7d396
unknown <malamminggu321@gmail.com>
2025-07-27 00:44:36 +0700

    searchbar

--- js/script.js
+++ js/script.js
@@ -1,17 +1,29 @@
-// toggle active
+// toggle active a menu
 const navbarNav = document.querySelector(".navbar-nav");
+//  toggle search
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
 
+document.querySelector("#search-button").onclick = () => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
 // ketika menu diklk
 document.querySelector("#a-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik random place for back
+// klik random
 
 const amenu = document.querySelector("#a-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
   if (!amenu.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
